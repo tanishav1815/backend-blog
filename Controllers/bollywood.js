431db8f18eab078231e1a17c052b2c3360976287
tanishav1815 <tanishav1815@gmail.com>
2022-11-24 01:24:02 +0530

    updated

--- Controllers/bollywood.js
+++ Controllers/bollywood.js
@@ -1,68 +1,43 @@
 const bollywood = (req,res)=>{
     res.send(
-        [
-            {
-                id: "bw4pid30ff84",
-                img1: "https://www.rollingstone.com/wp-content/uploads/2022/10/Jin-of-BTS_The-Astronaut-Concept-Photo_Outlander-1.jpg?w=1581&h=1054&crop=1",
-                Heading1: "Song You Need to Know: Jis “The Astronaut”",
-                sub1: "The dreamy track, co-written with Coldplay, is a gift for fans inspired by a powerful conversation Jin had with Chris Martin BTS Jin has been getting fans excited about his latest solo release, and it's finally here: The Astronaut is a dreamy track that he co-wrote with Coldplay, marking the second time BTS has collaborated with the British rock band, following the platinum 2021 hit ",
-                subh1: "Travel",
-                date1: "/ september 19 2022",
-              },
-              {
-                id: "bw5pid30ff84",
-                img1: "https://www.pngmart.com/files/22/Elon-Musk-PNG-Isolated-File.png",
-                Heading1: "Elon Musk Completes $44 Billion Deal to Own Twitter. Finally.",
-                sub1: "Elon Musk's Bid to Buy Twitter The world's richest man closed his blockbuster purchase of the social media service, thrusting Twitter into a new era. Send any friend a story As a subscriber, you have 10 gift articles to give each month. Anyone can read what you share.",
-                subh1: "Travel",
-                date1: "/ August 19 2022",
-              },
-              {
-                id: "bw6pid30ff84",
-                img1: "https://dn3bmh8yk8vvw.cloudfront.net/SZ6oqwCya1HXCtDlTsEGnrQioak=/production-17169-me9TLyxbJ7a5-ScreenShot20181126at3.48.27PM.png",
-                Heading1: "Police ram pair off scooter after 60mph pursuit",
-                sub1: "Police have released dramatic footage of officers ramming a scooter rider following a high-speed pursuit. The dashcam footage shows the Hampshire police vehicle pursuing a black Honda 125cc scooter at speeds of 60mph in a 30mph limit on The Avenue, Southampton.",
-                subh1: "Travel",
-                date1: "/ August 19 2022",
-              },
-              {
-                id: "104",
-          
-                img1: "https://assets.zerohedge.com/s3fs-public/styles/inline_image_mobile/public/inline-images/finlandtroops.png?itok=vdNg0xvm",
-                Heading1: "Finland Willing To Host NATO Nuclear Weapons On Border With Russia",
-                sub1: "Authored by Dave DeCamp via AntiWar.com, NATO nuclear weapons could potentially be placed on Russia's border with Finland if Helsinki joins the Western military alliance, reported on Wednesday, citing a Finnish newspaper. The newspaper Iltalehti reported that the bill the Finnish government will put before the country's parliament on joining NATO doesn't include any opt-outs for hosting nuclear weapons.",
-                subh1: "Travel",
-                date1: "/ August 19 2022",
-              },
-              {
-                id: "bw7pid30ff84",
-          
-                img1: "https://a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2022/10/720/405/Joe-Biden-Americans-Most-Important-Issues.jpg?ve=1&tl=1",
-                Heading1: "Biden daily gaffe average: The president is batting nearly a thousand",
-                sub1: "President Biden averaged nearly a gaffe per workday over the past four weeks, errors that included mispronunciations, confusion, inaccuracies, and forgetting that a congresswoman had died. Biden is hitting .810 from September 26 to October 24, committing at least 17 gaffes.",
-                subh1: "Travel",
-                date1: "/ August 19 2022",
-              },
-              {
-                id: "bw8pid30ff84",
-          
-                img1: "https://storage.googleapis.com/afs-prod/media/8fb6c4c48b3b47639bcb12e64eb7ebda/1000.jpeg",
-                Heading1: "Shanghai district orders mass COVID-19 testing, lockdown",
-                sub1: "BEIJING (AP) - China's largest city of Shanghai is ordering mass testing Friday on all 1.3 million residents of its downtown Yangpu district and confining them to their homes at least until results are known.",
-                subh1: "Travel",
-                date1: "/ August 19 2022",
-              },
-              {
-                id: "bw9pid30ff84",
-          
-                img1: "https://nypost.com/wp-content/uploads/sites/2/2022/10/hitler-kanye-comments-comp.jpg?quality=75&strip=all",
-                Heading1:
-                  "Kanye West has a long history of touting Hitler, sources reveal: report",
-                sub1: "A business executive who worked for Kanye West has alleged the rapper has long admired Adolf Hitler - and many close to him have been well aware.",
-                subh1: "Travel",
-                date1: "/ August 19 2022",
-              },
-            ],
+      [
+        {
+            "id":1,
+            "caption":"Shahruk Khan",
+            "details":"Shah Rukh Khan , also known by the initialism SRK, is an Indian actor, film producer, and television personality who works in Hindi films. Referred to in the media as the 'Baadshah of Bollywood' (in reference to his 1999 film Baadshah), 'King of Bollywood' and 'King Khan'",
+            "icon":"https://th.bing.com/th/id/OIP.9tWb9OCfS2JjpSXiKLfDpQHaJX?pid=ImgDet&rs=1"
+        },
+    
+        {
+            "id":2,
+            "caption":"Shraddha Kapoor",
+            "details":"Shraddha Kapoor (born 3 March 1987 or 1989 ) is an Indian actress who works in Hindi-language films. She features in listings of the most popular and highest-paid actresses in the country. She has been featured in Forbes India's Celebrity 100 list since 2014 and was featured by Forbes Asia in their 30 Under 30",
+            "icon":"https://images.mid-day.com/images/images/2014/dec/29-Shraddha-Kapoor_l.jpg"
+        },
+    
+        {
+            "id":3,
+            "caption":"Amitabh bachchan",
+            "details":"Amitabh Bachchan is an Indian actor, film producer, television host, occasional playback singer and former politician known for his work in Hindi cinema. He is regarded as one of the most influential actors in the history of Indian cinema.",
+            "icon":"https://th.bing.com/th/id/R.fbb47cb790f690050d25d88f9d34ac96?rik=NEROvMK3f6N1tQ&riu=http%3a%2f%2fwallsdesk.com%2fwp-content%2fuploads%2f2016%2f12%2fAmitabh-Bachchan-Background-.jpg&ehk=QzyG9aTUJ9KRkmvH5REmeIJoBborDOoiZXOZsgz%2brEE%3d&risl=&pid=ImgRaw&r=0"
+        },
+    
+        {
+            "id":4,
+            "caption":"deepika padukone",
+            "details":"Deepika Padukone is an Indian actress who works in Hindi films. She is one of the highest-paid actresses in India, and her accolades include three Filmfare Awards. She features in listings of the nation's most popular personalities, and Time named her one of the 100 most influential people in the world in 2018 ",
+            "icon":"https://th.bing.com/th/id/OIP.mF2tCDUJgBQPhaXvhbI9jwHaFc?pid=ImgDet&rs=1"
+        },
+    
+        {
+            "id":5,
+            "caption":"Anil Kapoor",
+            "details":"Anil Kapoor (born 24 December 1956) is an Indian actor and producer who works primarily in Hindi films, besides television and international films and television. In a career spanning over 40 years as an actor and since 2005 as a producer, Kapoor has appeared in more than 100 films.",
+            "icon":"https://i2.cinestaan.com/image-bank/1500-1500/59001-60000/59689.jpg"
+        }
+    
+    
+    ],
         
     )
 }
--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/' ,(req,res)=>{
     res.end()
 })
 
-app.listen(process.env.PORT||8080,()=>{
+app.listen(process.env.PORT||3000,()=>{
     console.log("server running");
 })
