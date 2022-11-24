913cea48e41c9f9824a87e464ba846bdd4a7bda8
tanishav1815 <tanishav1815@gmail.com>
2022-11-24 11:20:23 +0530

    four

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const data1 = require("./Routes/bollywood")
+const data3 = require("./Routes/hollywood")
 
 app.use('/home', data1)
+app.use('/hollywood',data3)
 
 app.get('/' ,(req,res)=>{
     res.write("hello");
