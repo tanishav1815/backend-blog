6bebd98cc04e9750a2cfa59dbc1465ce327e549a
tanishav1815 <tanishav1815@gmail.com>
2022-11-24 11:03:25 +0530

    third

--- Routes/Hollywood.js
+++ Routes/Hollywood.js
@@ -6,6 +6,6 @@ const data3 = require("../Controllers/hollywood");
 
 const data3Router = express.Router()
 data3Router.route('/hollywood')
-.get(data3.HollywoodData)
+.get(data3.hollywoodData)
 
 module.exports=data3Router;
